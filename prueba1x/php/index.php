fd94cda6a8f6a02df910868d61f73489b4d85097
Mye270419 <127803889+Mye270419@users.noreply.github.com>
2023-11-09 17:18:22 -0400

    Add files via upload

---
+++ prueba1x/php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Página de Prueba</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Mi Página de Prueba</h1>
+    </header>
+    <main>
+        <h2>Bienvenido a mi página de prueba</h2>
+        <p>Esta es una página HTML simple con algunos estilos CSS básicos.</p>
+    </main>
